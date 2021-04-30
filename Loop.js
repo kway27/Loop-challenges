9878dd980ba5d8bf38b51e16e9bb7312269bc237
Daniel Amponsah <dakway27@gmail.com>
2021-04-30 01:03:51 -0400

    Loop Challenges

---
+++ Loop.js
@@ -0,0 +1,16 @@
+for(var i=1; i<20; i+=2) {
+    console.log(i);
+}
+
+for(var i=4; i>-4; i-=1.5) {
+    console.log(i);
+}
+
+var sum = 0;
+for( var i = 0; i < elmt.length; i++ ){
+    console.log(sum);
+}
+
+for(var i=1; i<le8; i*n) {
+    console.log(i);
+}
